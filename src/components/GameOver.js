1bde47ceb883ceebb146d6192b8b3320c3f12393
HarshitMadan77 <harshitmadan77@gmail.com>
2024-05-03 14:31:14 +0530

    feat: play again button

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -3,11 +3,18 @@ import { appContext } from '../App'
 
 function GameOver() {
     const { gameOver, currAttempt, correctWord } = useContext(appContext);
+
+    const handleRestart = () => {
+        // Reload the page to restart the game
+        window.location.reload();
+    };
+
     return (
         <div className='gameOver'>
             <h3>{gameOver.guessedWord ? "You Guessed Correctly !!" : "You Lost !!"}</h3>
             <h1>Correct Word: {correctWord}</h1>
             {gameOver.guessedWord && (<h3>You Guessed in {currAttempt.attempt} attempts</h3>)}
+            <button className="playAgainButton" onClick={handleRestart}>Play Again</button>
         </div>
     )
 }
